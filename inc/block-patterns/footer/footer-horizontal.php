6e0b760480513c6c3395e6907f0c80c17ed1df6f
Anders <anders@andersnoren.se>
2022-07-15 17:52:20 +0200

    Progress

--- inc/block-patterns/footer/footer-horizontal.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-/*
- * This is the default footer. Instead of including the HTML for it twice,
- * we include the HTML in the footer.html block template part file.
- */
-
-include( locate_template( 'parts/footer.html' ) );
--- inc/block-patterns/header/header-horizontal.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-/*
- * This is the default header. Instead of including the HTML for it twice,
- * we include the HTML in the header.html block template part file.
- */
-
-include( locate_template( 'parts/header.html' ) );
